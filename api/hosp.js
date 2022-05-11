765ee377037e609a85246795bd7b2653f84d3408
zhangzian <zhangza@op-mobile.com.cn>
2022-05-11 10:25:33 +0800

    yygh-site 会员系统前端

---
+++ api/hosp.js
@@ -0,0 +1,58 @@
+import request from '@/utils/request'
+
+const api_name = `/api/hosp/hospital`
+
+export default {
+ getPageList(page, limit, searchObj) {
+    return request({
+        url: `${api_name}/findHospList/${page}/${limit}`,
+        method: 'get',
+        params: searchObj
+    })
+  },
+
+ getByHosname(hosname) {
+    return request({
+        url: `${api_name}/findByHosname/${hosname}`,
+        method: 'get'
+    })
+  },
+  //根据医院编号查询医院详情信息
+  show(hoscode) {
+      return request({
+          url: `${api_name}/findHospDetail/${hoscode}`,
+          method: 'get'
+      })
+  },
+  //根据医院编码查询科室
+  findDepartment(hoscode) {
+      return request({
+          url: `${api_name}/department/${hoscode}`,
+          method: 'get'
+      })
+  },
+  //获取可预约的信息 分页
+  getBookingScheduleRule(page, limit, hoscode, depcode) {
+      return request({
+          url: `${api_name}/auth/getBookingScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+          method: 'get'
+      })
+  },
+
+  //根据日期获取详情信息
+  findScheduleList(hoscode, depcode, workDate) {
+      return request({
+          url: `${api_name}/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`,
+          method: 'get'
+      })
+  },
+  getSchedule(id) {
+  return request({
+    url: `${api_name}/getSchedule/${id}`,
+    method: 'get'
+  })
+  }
+
+
+
+}
---
+++ api/userInfo.js
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+
+const api_name = `/api/user`
+
+export default {
+    login(userInfo) {
+        return request({
+            url: `${api_name}/login`,
+            method: `post`,
+            data: userInfo
+        })
+    },
+    //根据userId获取用户信息
+    getUserInfo() {
+    return request({
+      url: `${api_name}/auth/getUserInfo`,
+      method: `get`
+    })
+    },
+    //用户认证
+    saveUserAuah(userAuah) {
+      return request({
+        url: `${api_name}/auth/userAuth`,
+        method: 'post',
+        data: userAuah
+      })
+    }
+
+}
